ce059383c543b0f9ce886073ad4ce31eeb8056f5
v.kishore <velakiya11@gmail.com>
2026-03-11 22:32:25 +0530

    Update code.js

--- code.js
+++ code.js
@@ -10,14 +10,20 @@ document.body.innerHTML='kishore';
 document.body.innerHTML=' ';
 Math.round((18.50+7.50+10)/0.1);
 
-
+//math
 let numi=2.8;
 let num2=2.2;
 let round1=Math.floor(num1);
 let round2=Math.ceil(num2);
 console.log(round1);
 console.log(round2);
-
+//string
 console.log('my name is');
 console.log('kishore');
 console.log('my name is'+' '+'kishore');
+//string concantenation
+
+let coffee=5;
+let bagal=3;
+let total= coffee + bagal;
+console.log('total cost:'+' '+total);
