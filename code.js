8f29c3fa705bd136aecf886579128ae1251c950b
v.kishore <velakiya11@gmail.com>
2026-03-19 23:42:42 +0530

    Update code.js

--- code.js
+++ code.js
@@ -84,3 +84,16 @@ document.body.innerHTML=' ';
 Math.round((18.50+7.50+10)/0.1);
 
 //math
+Math.round(13878.37633575+8368.7464691*9963194.881479);
+alert('good gref');
+document.body.innerHTML='arigatho';
+document.body.innerHTML='sayonara';
+alert('ohaiho sensei');
+alert('goog');
+alert('kishore');
+document.body.innerHTML='good';
+document.body.innerHTML='kishore';
+document.body.innerHTML=' ';
+Math.round((18.50+7.50+10)/0.1);
+
+//math
